40978071eeae9cda2fb876a9bf88053d11bc1b56
@gmgower <gmgower@gmail.com>
2020-07-27 16:55:14 -0500

    Navbar components setup.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './App.scss'
-import "@fortawesome/fontawesome-free/css/all.min.css"
+import Navbar from './components/Navbar/Navbar'
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>App</h1>
-      </div>
+      <React.Fragment>
+        <Navbar />
+      </React.Fragment>
     )
   }
 }
